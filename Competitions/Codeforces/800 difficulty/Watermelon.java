ac134648e3d3d85d88d7b0b79c5a3eef7ea68db0
TheIshanD <56279050+TheIshanD@users.noreply.github.com>
2020-11-28 13:45:13 -0600

    Create Watermelon.java

---
+++ Competitions/Codeforces/800 difficulty/Watermelon.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+ 
+public class Problem1 {
+    public static void main (String [] args) throws IOException {
+        BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        //--------------------------START CODING HERE-----------------------------
+ 
+        StringTokenizer st = new StringTokenizer(f.readLine());
+        int N = Integer.parseInt(st.nextToken());
+ 
+        if(N%2 ==0)
+        {
+            if(N <= 3)
+            {
+                System.out.println("No");
+            }
+            else
+                {
+                    if((N -2) % 2 == 0)
+                    {
+                        System.out.println("YES");
+                    }
+                    else
+                    {
+                        System.out.println("No");
+                    }
+                }
+ 
+        }
+        else out.print("No");
+        out.close();
+    }
