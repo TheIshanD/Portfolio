562e05b9a979f1dca419a5c6dfb2d45239921640
TheIshanD <56279050+TheIshanD@users.noreply.github.com>
2020-11-28 15:17:38 -0600

    Create Make It Good.java

---
+++ Competitions/Codeforces/1200 difficulty/Make It Good.java
@@ -0,0 +1,97 @@
+import javax.management.Query;
+import javax.print.attribute.SupportedValuesAttribute;
+import java.io.*;
+import java.security.interfaces.DSAKeyPairGenerator;
+import java.sql.Array;
+import java.util.*;
+import java.util.function.Predicate;
+ 
+ 
+public class Main {
+ 
+    static class TaskB {
+ 
+        static int oo = Integer.MAX_VALUE - 1;
+ 
+        static int MOD = 1000000007;
+        public void solve(InputReader in, PrintWriter out) {
+            int T = in.nextInt();
+            for(int tt = 0; tt < T; tt++)
+            {
+                int n = in.nextInt();
+ 
+                int[] arr = in.nextIntArray(n);
+ 
+                int index = n-1;
+                for(;index>0; index--)
+                {
+                    if(arr[index]>arr[index-1])break;
+                }
+                for(;index>0; index--)
+                {
+                    if(arr[index]<arr[index-1])break;
+                }
+                out.println(index);
+            }
+ 
+ 
+        }
+    }
+ 
+    public static void main(String[] args) {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        TaskB solver = new TaskB();
+        solver.solve(in, out);
+        out.close();
+    }
+ 
+ 
+ 
+    static void sort(int[] a)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(Integer i: a)list.add(i);
+        Collections.sort(list);
+        for(int i = 0; i < a.length; i++)a[i]=list.get(i);
+    }
+ 
+ 
+    static class InputReader {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer = new StringTokenizer("");
+ 
+        public InputReader(InputStream inputStream) {
+            this.reader = new BufferedReader(
+                    new InputStreamReader(inputStream));
+        }
+ 
+        public String next() {
+            while (!tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+ 
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = nextInt();
+            return a;
+        }
+    }
+}
